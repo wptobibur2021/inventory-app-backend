32987982edb263fdc261984c0ae6b6181b1f306a
Md Tobibur Rohman <71486299+wptobibur2021@users.noreply.github.com>
2022-03-13 23:02:59 +0600

    Heroku

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const userRoute = require('./Routers/user')
  *  ======================
  */
 app.use(cors(), function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://inventory-app-2022.web.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
